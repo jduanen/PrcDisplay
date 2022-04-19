3a98df1f71a1450435eb9347dd39fe9500da2c7b
Duane Northcutt <duane.northcutt@gmail.com>
2022-04-18 17:26:12 -0700

    Fixed typos.

--- libraries/LedArray/examples/Test/Test.ino
+++ libraries/LedArray/examples/Test/Test.ino
@@ -4,6 +4,7 @@
  * 
  */
 
+#include <Arduino.h>
 #include <LedArray.h>
 
 
@@ -15,14 +16,14 @@
 #define NUM_ROWS        7
 #define NUM_COLS        21
 
-#define STD_WAIT        100 //50
+#define STD_WAIT        10
 
 #define WIDE_FONT           0
 #define SKINNY_FONT         1
 #define VERY_SKINNY_FONT    2
 
 
-LedArray leds = LedArray<NUM_SR>(DATA_PIN, SRCLK_PIN, RCLK_PIN, NUM_ROWS, NUM_COLS, NUM_SR, STD_WAIT);
+LedArray<NUM_SR> leds(DATA_PIN, SRCLK_PIN, RCLK_PIN, NUM_ROWS, NUM_COLS, NUM_SR, STD_WAIT);
 
 
 void setup() { 
@@ -33,11 +34,11 @@ void setup() {
   //// TODO get and print available font info
 
   String msg = String("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789");
-  leds.message(msg, SKINNY_FONT);
+  leds.message(&msg, SKINNY_FONT);
 }
 
 void loop() {
   leds.run();
-  printf("Loop");
+  Serial.println("Loop");
   delay(1000);
 }
