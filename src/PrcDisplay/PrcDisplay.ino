7dd433f792b7de5f2575647862c9e9e82fe3a8ef
Duane Northcutt <duane.northcutt@gmail.com>
2022-04-25 15:28:09 -0700

    Added web services feature.

--- src/PrcDisplay/PrcDisplay.ino
+++ src/PrcDisplay/PrcDisplay.ino
@@ -52,8 +52,6 @@ void println(String str) {
 
 #define WEB_SERVER_PORT     80
 
-AsyncWebServer  server(WEB_SERVER_PORT);
-
 #define I2C_BASE_ADDR       0x38    // PCF8574A
 //#define I2C_BASE_ADDR       0x20    // PCF8574
 #define READ_ADDR           0x4F
@@ -61,14 +59,256 @@ AsyncWebServer  server(WEB_SERVER_PORT);
 
 #define NUM_EL_WIRES        8
 
+#define TEST_NUMBER         1   //// TMP TMP TMP
+
+#define DATA_PIN            14  // D5
+#define SRCLK_PIN           12  // D6
+#define RCLK_PIN            13  // D7
+#define OE_PIN              15  // D8
+
+#define NUM_SR              4
+#define NUM_ROWS            7
+#define NUM_COLS            21
+
+#define STD_WAIT            35
+
+#define WIDE_FONT           '0'
+#define SKINNY_FONT         '1'
+#define VERY_SKINNY_FONT    '2'
+#define SYMBOLS_FONT        '3'
+
 //#define SCL_PIN             1
 //#define SDA_PIN             2
 
-PCF8574 prcd = PCF8574(I2C_BASE_ADDR);
 
 bool enableELwires = true;
+bool enableLedArray = true;
+const int ledPin = 2;
+
+AsyncWebServer  server(WEB_SERVER_PORT);
+AsyncWebSocket  ws("/ws");
+
+PCF8574 prcd = PCF8574(I2C_BASE_ADDR);
+
+LedArray<NUM_SR> leds(DATA_PIN, SRCLK_PIN, RCLK_PIN, OE_PIN, NUM_ROWS, NUM_COLS, STD_WAIT);
 
 byte lastWires = 0;
+
+String fontNamesList = String();
+
+const char index_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE HTML><html>
+<head>
+  <title>PrcDisplay Web Server</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" href="data:,">
+  <style>
+  html {
+    font-family: Arial, Helvetica, sans-serif;
+    text-align: center;
+  }
+  h1 {
+    font-size: 1.8rem;
+    color: white;
+  }
+  h2{
+    font-size: 1.5rem;
+    font-weight: bold;
+    color: #143642;
+  }
+  h3{
+    font-size: 1.3rem;
+    font-weight: bold;
+    color: #143642;
+  }
+  .topnav {
+    overflow: hidden;
+    background-color: #143642;
+  }
+  body {
+    margin: 0;
+  }
+  .content {
+    padding: 30px;
+    max-width: 600px;
+    margin: 0 auto;
+  }
+  .card {
+    background-color: #F8F7F9;;
+    box-shadow: 2px 2px 12px 1px rgba(140,140,140,.5);
+    padding-top:10px;
+    padding-bottom:20px;
+  }
+  .button {
+    padding: 15px 50px;
+    font-size: 24px;
+    text-align: center;
+    outline: none;
+    color: #fff;
+    background-color: #0f8b8d;
+    border: none;
+    border-radius: 5px;
+    -webkit-touch-callout: none;
+    -webkit-user-select: none;
+    -khtml-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+    -webkit-tap-highlight-color: rgba(0,0,0,0);
+   }
+   /*.button:hover {background-color: #0f8b8d}*/
+   .button:active {
+     background-color: #0f8b8d;
+     box-shadow: 2 2px #CDCDCD;
+     transform: translateY(2px);
+   }
+   .staticState {
+     font-size: 1.2rem;
+     color:black;
+     font-weight: bold;
+   }
+   .state {
+     font-size: 1.2rem;
+     color:#8c8c8c;
+     font-weight: bold;
+   }
+  </style>
+<title>PrcDisplay Web Server</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="icon" href="data:,">
+</head>
+<body>
+  <div class="topnav">
+    <h1>PrcDisplay WebSocket Server</h1>
+  </div>
+  <div class="content">
+    <div class="card">
+      <h2>System Information</h2>
+      <p class="staticState">App Version: <span style="color:blue" id="version">%VERSION%</span></p>
+      <p class="staticState">IP Address: <span style="color:blue" id="ipAddr">%IP_ADDR%</span></p>
+      <p class="staticState">Connection: <span style="color:blue" id="connection">%CONNECTION%</span></p>
+      <p class="staticState">RSSI: <span style="color:blue" id="rssi">%RSSI%</span></p>
+      <p class="staticState">LED Fonts Version: <span style="color:blue" id="fontsVersion">%FONTS_VERSION%</span></p>
+      <p class="staticState">Number of LED Fonts: <span style="color:blue" id="numFonts">%NUMBER_OF_FONTS%</span></p>
+      <p class="staticState">LED Font Names: <span style="color:blue" id="fontsNames">%FONT_NAMES%</span></p>
+    </div>
+    <br>
+    <div class="card">
+      <h2>Controls</h2>
+      <h3>System</h3>
+      <p class="state">Update Firmware: <a href="update">Update</a></p>
+      <h3>LED Array</h3>
+      <p class="state">LED Array: <span id="ledState">%LED_STATE%</span> 
+      <button id="button" class="button">Toggle</button></p>
+      <h3>EL Wires</h3>
+      <p class="state">EL Wires: <span id="elState">%EL_STATE%</span> 
+      <button id="button" class="button">Toggle</button></p>
+    </div>
+  </div>
+<script>
+  var gateway = `ws://${window.location.hostname}/ws`;
+  var websocket;
+  window.addEventListener('load', onLoad);
+  function initWebSocket() {
+    console.log('Trying to open a WebSocket connection...');
+    websocket = new WebSocket(gateway);
+    websocket.onopen    = onOpen;
+    websocket.onclose   = onClose;
+    websocket.onmessage = onMessage; // <-- add this line
+  }
+  function onOpen(event) {
+    console.log('Connection opened');
+  }
+  function onClose(event) {
+    console.log('Connection closed');
+    setTimeout(initWebSocket, 2000);
+  }
+  function onMessage(event) {
+    var state;
+    console.log('event: ', event);
+    if (event.data == "1"){
+      state = "ON";
+    }
+    else{
+      state = "OFF";
+    }
+    document.getElementById('ledState').innerHTML = state;
+  }
+  function onLoad(event) {
+    initWebSocket();
+    initButton();
+  }
+  function initButton() {
+    document.getElementById('button').addEventListener('click', toggle);
+  }
+  function toggle(){
+    websocket.send('toggle');
+  }
+</script>
+</body>
+</html>)rawliteral";
+
+
+void notifyClients() {
+  ws.textAll(String(enableLedArray));
+}
+
+void handleWebSocketMessage(void *arg, uint8_t *data, size_t len) {
+  AwsFrameInfo *info = (AwsFrameInfo*)arg;
+  if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT) {
+    data[len] = 0;
+    if (strcmp((char*)data, "toggle") == 0) {
+      enableLedArray = !enableLedArray;
+      notifyClients();
+    }
+  }
+}
+
+void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type,
+             void *arg, uint8_t *data, size_t len) {
+  switch (type) {
+    case WS_EVT_CONNECT:
+      Serial.printf("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
+      break;
+    case WS_EVT_DISCONNECT:
+      Serial.printf("WebSocket client #%u disconnected\n", client->id());
+      break;
+    case WS_EVT_DATA:
+      handleWebSocketMessage(arg, data, len);
+      break;
+    case WS_EVT_PONG:
+    case WS_EVT_ERROR:
+      break;
+  }
+}
+
+void initWebSocket() {
+  ws.onEvent(onEvent);
+  server.addHandler(&ws);
+}
+
+String processor(const String& var){
+  if (var == "VERSION") {
+    return (APP_VERSION);
+  } else if (var == "IP_ADDR") {
+    return (WiFi.localIP().toString());
+  } else if (var == "CONNECTION") {
+    return (WiFi.SSID());
+  } else if (var == "RSSI") {
+    return (String(WiFi.RSSI()));
+  } else if (var == "FONTS_VERSION") {
+    return (FONTS_VERSION);
+  } else if (var == "NUMBER_OF_FONTS") {
+    return (String(NUM_FONTS));
+  } else if (var == "FONT_NAMES") {
+    return (fontNamesList);
+  } else if (var == "LED_STATE") {
+    return (enableLedArray ? "ON" : "OFF");
+  } else if (var == "EL_STATE") {
+    return (enableELwires ? "ON" : "OFF");
+  }
+  return String();
+}
 
 void initWires() {
   prcd.pinMode(P0, OUTPUT, HIGH);
@@ -102,39 +342,19 @@ void writeAllWires(byte values) {
   print("writeAllWires: 0x" +  String(values, HEX) + "; ");
 }
 
-#define TEST_NUMBER         1   //// TMP TMP TMP
-
-#define DATA_PIN            14  // D5
-#define SRCLK_PIN           12  // D6
-#define RCLK_PIN            13  // D7
-#define OE_PIN              15  // D8
-
-#define NUM_SR              4
-#define NUM_ROWS            7
-#define NUM_COLS            21
-
-#define STD_WAIT            35
-
-#define WIDE_FONT           '0'
-#define SKINNY_FONT         '1'
-#define VERY_SKINNY_FONT    '2'
-#define SYMBOLS_FONT        '3'
-
-LedArray<NUM_SR> leds(DATA_PIN, SRCLK_PIN, RCLK_PIN, OE_PIN, NUM_ROWS, NUM_COLS, STD_WAIT);
-
-bool enableLedArray = true;
-
 //// FIXME
 void initLedArray() {
   String msg = String("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789");
 
   println("Fonts Version: " + String(FONTS_VERSION));
   println("Number of Fonts: " + String(NUM_FONTS));
-  print("Font Names: [");
   for (int i = 0; (i < NUM_FONTS); i++) {
-    print(fontNames[i] + String(", "));
+    if (i > 0) {
+      fontNamesList += String(", ");
+    }
+    fontNamesList += fontNames[i];
   }
-  println("]");
+  println("Font Names: [" + fontNamesList + "]");
 
   leds.clear();
   println("Running Test #" + String(TEST_NUMBER));
@@ -168,39 +388,48 @@ void initLedArray() {
   }
 }
 
-
 void setup() { 
   delay(500);
   Serial.begin(19200);
   delay(500);
   Serial.println("\nBEGIN");
 
+  //// TMP TMP TMP
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
+  
   WiFi.mode(WIFI_STA);
   WiFi.begin(WLAN_SSID, WLAN_PASS);
-  Serial.println("Connecting ");
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    print(".");
+    Serial.println("Connecting to WiFi..");
   }
   Serial.println("\nConnected to " + String(WLAN_SSID));
   Serial.println("IP address: " + WiFi.localIP().toString());
 
-  //// FIXME
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
-    request->send(200, "text/plain", "PrcDisplay");
+  initWebSocket();
+
+  // route for root / web page
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/html", index_html, processor);
   });
 
   AsyncElegantOTA.begin(&server);
   server.begin();
-  println("HTTP server started");
+  Serial.println("HTTP server started");
 
   initWires();
+  Serial.println("EL wires function started");
 
   initLedArray();
+  Serial.println("LED array function started");
 }
 
 void loop() {
-  //// web interface
+  ws.cleanupClients();
+
+  //// TMP TMP TMP
+  digitalWrite(ledPin, enableLedArray);
 
   if (enableELwires) {
     byte wireVals = (1 << ((loopCnt >> 10) % 8));
