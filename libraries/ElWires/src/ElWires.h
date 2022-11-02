c288bb01721ab2c096e4e4773dfdf5ab4627d00e
Duane Northcutt <duane.northcutt@gmail.com>
2022-11-01 17:06:21 -0700

    Changed from cycle count delays to time-based ones.

--- libraries/ElWires/src/ElWires.h
+++ libraries/ElWires/src/ElWires.h
@@ -11,7 +11,7 @@
 #define EL_WIRE_LIB_VERSION     "1.0"
 
 #define DEF_SEQUENCE_NUMBER 0
-#define DEF_SEQUENCE_SPEED  0
+#define DEF_SEQUENCE_DELAY  100
 
 #define I2C_BASE_ADDR       0x38    // PCF8574A afd driver
 //#define I2C_BASE_ADDR       0x20    // PCF8574 breakout board
@@ -33,12 +33,12 @@ public:
 
 	unsigned short numSequences();
 	unsigned short sequenceNumber();
-	unsigned int sequenceSpeed();
-	void setSequence(unsigned short number, unsigned int speed);
+	uint32_t sequenceDelay();
+	void setSequence(unsigned short number, uint32_t delay);
 
 	void clear();
 	void writeAll(byte values);
-	void run();
+	unsigned long run();
 
 private:
 	PCF8574 _prcd = PCF8574(I2C_BASE_ADDR);
@@ -46,16 +46,13 @@ private:
 	uint8_t _numElWires;
 	unsigned short _numSequences;
 
-	unsigned int _delayCycles = 0;
-	unsigned int _seqIndx = 0;
+	unsigned short _sequenceNumber = DEF_SEQUENCE_NUMBER;
+	uint32_t _sequenceDelay = DEF_SEQUENCE_DELAY;
 
 	bool _randomSequence = false;
-	unsigned short _sequenceNumber = DEF_SEQUENCE_NUMBER;
-	unsigned int _sequenceSpeed = DEF_SEQUENCE_SPEED;
 
-	bool _lastRandomSequence = _randomSequence;
-	unsigned short _lastSequenceNumber = _sequenceNumber;
-	unsigned int _lastSequenceSpeed = _sequenceSpeed;
+    unsigned long _nextRunTime = 0;
+	unsigned int _seqIndx = 0;
 
 	void _create(bool delayedStart);
 };
